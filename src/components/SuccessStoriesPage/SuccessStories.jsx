a5c91b79f69ca246e41e429d48d1878fe3fcbb1b
elencrisf <elen.crisf@uol.com.br>
2020-07-10 19:02:15 -0700

    Second Commit Success

--- src/components/SuccessStoriesPage/SuccessStories.jsx
+++ src/components/SuccessStoriesPage/SuccessStories.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import "./successStories.scss"
 import Image from "../Image"
-// import { StyledButton } from "../../Elements/Elements"
-// import { Link } from "gatsby"
+import { StyledButton } from "../Elements/Elements"
+import { Link } from "gatsby"
 // import { Card } from "react-bootstrap"
 // import { useStaticQuery, graphql } from "gatsby"
 
@@ -21,31 +21,98 @@ const SuccessStories = () => {
   return (
     <>
     <div className="stories">
-      {/* <div className="stories__image">
-        <Image filename={"sample1.jpg"} />
-      </div> */}
-      {/* <p className="stories__title">Success stories here</p> */}
-      <div className="card mb-3" style={{maxWidth: "540px"}}>
-      <div className="row no-gutters">
-        <div className="col-md-4">
-          <Image 
-          filename={"sample1.jpg"} 
-          className="card-img" 
-          alt=""
-          />
+      <div className="stories__title">
+        <h1>Stories from our drivers of change to inspire your own business success</h1>
+      </div>
+      <div className="stories__subtitle">
+        <h4>Discover how enterprises and organizations have expanded their reach and gained more supporters with help from Impactraction.</h4>
+      </div>
+      <div className="stories__card">
+        <div className="card mb-3" style={{ width: "100%"}}>
+          <div className="row no-gutters">
+            <div className="col-md-5">
+              <div className="card-body">
+                <h3 className="card-title">We Volunteer</h3>
+                <br></br>
+                <p className="card-text">We Volunteer stands for an active society. We want to make
+                  volunteering part of everyone’s daily life by making it easy and
+                  rewarding.</p>
+                <p>Inspired by the willingness to do more good, in August 2017 Malte
+                  founded We Volunteer as an online platform to connect people
+                  and corporations to volunteering projects and social initiatives.
+                  Anyone can easily find a social project matching their skills and
+                  interests and get involved. Since 2015, We Volunteer has grown in
+                  size and scope and connected 250,000 unique users to more than
+                  3,500 volunteer opportunities in 250 cities across Canada.</p>
+                {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
+                <Link to="/">
+                  <StyledButton>Read More</StyledButton>
+                </Link>
+              </div>
+            </div>
+            <div className="col-md-7">
+              <Image 
+              filename={"sucessStories1.png"} 
+              className="card-img" 
+              alt=""
+              />
+            </div>
+          </div>
         </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. 
-              This content is a little bit longer.</p>
-            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <Image 
+              filename={"sucessStories2.png"} 
+              className="card-img-top" 
+              alt=""
+              />
+              <div className="card-body">
+                <h3 className="card-title">Green Earth</h3>
+                <p className="card-text">The Climate Action Network was founded with the goal of uniting
+                  climate activists into a movement, with a strategy of bottom-up
+                  organizing local-climate-focused campaigns and projects around the
+                  world. Green Earth is an educational and research organization
+                  dedicated to the healing and harmonizing of the relationships between
+                  humanity and the earth. Our objectives are to help bring about changes
+                  in attitudes, values, perceptions, and world views that are based on
+                  ecological balance and respect for all life-forms on earth.</p>
+                <Link to="/">
+                  <StyledButton>Read More</StyledButton>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card">
+              <Image 
+              filename={"sucessStories3.png"} 
+              className="card-img-top" 
+              alt=""
+              />
+              <div className="card-body">
+                <h3 className="card-title">Aid:Water</h3>
+                <p className="card-text">663 million people in the world live without clean water. At Aid:Water,
+                  the mission is to change that by bringing clean and safe drinking water
+                  to people in developing countries. The organization works with dozens
+                  of local partners overseas, and has completed over 21,000 water
+                  projects to bring more than 6.4 million people access to water, hygiene,
+                  and improved sanitation. Aid:Water transforms the lives of the poorest
+                  and most marginalized people by improving access to clean water, safe
+                  toilets, and hygiene programs.</p>
+                <Link to="/">
+                  <StyledButton>Read More</StyledButton>
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-      {/* <Link to="/successStories">
-        <StyledButton>Link to Success Stories</StyledButton>
+      <div className="stories__footer">
+        <h2>Share your innovative ideas and tackle social challenges with us.</h2>
+      </div>
+      {/* <Link to="/">
+        <StyledButton>Read More</StyledButton>
       </Link> */}
     </div>
     </>
--- src/pages/successStories.js
+++ src/pages/successStories.js
@@ -2,12 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
 import SuccessStories from "../components/SuccessStoriesPage/SuccessStories"
+import { StyledButton } from "../components/Elements/Elements"
 
 const successStories = () => (
   <>
     <SEO title="Success Stories" />
     <SuccessStories />
-    <Link to="/">Go back to the homepage</Link>
+    
+    <Link to="/">
+    <StyledButton>Go back to the homepage</StyledButton>
+    </Link>
   </>
 )
 
