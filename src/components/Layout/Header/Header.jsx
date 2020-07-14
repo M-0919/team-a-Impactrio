c03579186bab7aa589922fc50b5ab96830973670
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-07-14 14:06:31 -0700

    demo:navbar for henry

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import "./Header.scss"
 import Image from "../../Image"
 import { Link } from "gatsby"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
 // import { useStaticQuery, graphql } from "gatsby"
 
 const Header = () => {
@@ -39,7 +41,7 @@ const Header = () => {
         </li>
       </ul> */}
       {/* <h2 className="header__title">Header here</h2> */}
-      <nav className="navbar navbar-expand-lg navbar-light wrap">
+      {/* <nav className="navbar navbar-expand-lg navbar-light wrap">
         <Link to="/">
           <div className="header__logo">
             <Image filename={"logoA.png"} alt="logo" />
@@ -72,7 +74,30 @@ const Header = () => {
             </Link>
           </div>
         </div>
-      </nav>
+      </nav> */}
+      <Navbar collapseOnSelect expand="lg">
+        <Navbar.Brand href="#home">
+          <div className="header__logo">
+            <Image filename={"logoA.png"} alt="logo" />
+          </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link>
+              <Link to="/howItWorks">How It Works</Link>
+            </Nav.Link>
+            <Nav.Link eventKey={2}>
+              <Link to="/aboutUs">About Us</Link>
+            </Nav.Link>
+            <Nav.Link>More deets</Nav.Link>
+            <Nav.Link eventKey={2}>
+              <Link to="/successStories">Success Stories</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </header>
   )
 }
